14df9df030264e168037b0429f16126307e7b6c6
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-05 15:09:59 -0500

    added index file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//const Engineer; require path to engineer class
+// we need to bring in our file system
+// we need our paths
+// we need inquirer
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquierer');
+
+// we need our extensions of our Employee class
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
