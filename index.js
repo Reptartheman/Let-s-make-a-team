61de654994b2044f7b2c4be6017e07f52e5f31d8
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-05 15:28:06 -0500

    added packages, fixed some typos and added a simple test question

--- index.js
+++ index.js
@@ -3,11 +3,28 @@
 // we need inquirer
 const fs = require('fs');
 const path = require('path');
-const inquirer = require('inquierer');
+const inquirer = require('inquirer');
 
 // we need our extensions of our Employee class
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+
+const testQuestions = {
+    type: 'list', 
+    name: 'Simple test',
+    message: 'Is the question showing up in the console?',
+    choices: [
+        'Yes',
+        'No',
+    ]
+}
+
+const init = () => {
+    inquirer.prompt(testQuestions)
+}
+
+init();
 
