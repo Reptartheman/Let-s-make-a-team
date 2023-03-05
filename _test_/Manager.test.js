c39d0b993afab8fda75153d96f23b6ecd1aaf770
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-05 18:42:56 -0500

    Added Manager test file

--- _test_/Manager.test.js
+++ _test_/Manager.test.js
@@ -2,6 +2,25 @@ const Manager = require('../lib/Manager');
 
 describe('The manager', () => {
     describe('Manager', ()=> {
-        it('should ')
-    })
-})
+        it('should create an object with the properties from the Manager subclass', () => {
+            const manager = new Manager('Bob', '4', 'bob@bob.com', '7');
+
+            expect(manager.name).toEqual('Bob');
+            expect(manager.id).toEqual('4');
+            expect(manager.email).toEqual('bob@bob.com');
+            expect(manager.officeNumber).toEqual('7');
+        });
+    });
+
+    describe('getRole function from Manager class', () => {
+        it('should return the Managers role at the company', () => {
+            expect(new Manager('Bob', '4','bob@bob.com', '7').getRole().toBe('Manager'));
+        });
+    });
+
+    describe('officeNumber function form the Manager class', () => {
+        it('should return the Managers office number', () => {
+            expect(new Manager('Bob', '4','bob@bob.com', '7').getOfficeNumber().toBe('7'));
+        });
+    });
+});
