80de530763ddf94b5a2e32ccd9e823cd59277aa1
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-05 16:44:19 -0500

    Employee test working

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -6,11 +6,33 @@ describe('Employee class', () => {
         it('should create a new object that shows a name, id, and email', () => {
             const employee = new Employee('Bob','4', 'bob@bob.com');
 
-            expect(typeof employee.name).toEqual('Bob')
-            expect(typeof employee.id).toEqual('4')
-            expect(typeof employee.email).toEqual('bob@bob.com')
+            expect(employee.name).toEqual('Bob')
+            expect(employee.id).toEqual('4')
+            expect(employee.email).toEqual('bob@bob.com')
         });
     });
 
-    
-})
+    describe('getName', () => {
+        it('should return the persons name', () => {
+            expect(new Employee('Bob', '4', 'bob@bob.com').getName()).toBe('Bob');
+        });
+    });
+
+    describe('getId', () => {
+        it('should return the id', () => {
+            expect(new Employee ('Bob', '4', 'bob@bob.com').getId()).toBe('4');
+        });
+    });
+
+    describe('getEmail', () => {
+        it('should return the employee email', () => {
+            expect(new Employee ('Bob', '4', 'bob@bob.com').getEmail()).toBe('bob@bob.com');
+        });
+    });
+
+    describe('getRole', () => {
+        it('should give the employees job', () => {
+            expect(new Employee ('Bob', '4', 'bob@bob.com').getRole()).toBe('Employee');
+        });
+    });
+});
