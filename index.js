44c5d92660fec400d6144f5040f0cdbfbcfd1658
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-03-08 17:45:30 -0500

    Added questions and team array

--- index.js
+++ index.js
@@ -14,3 +14,73 @@ const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generateHTML');
 
 
+const arrayForTeam = [];
+
+
+
+function startApp(){
+    console.log('Hello, welcome to the team creator app! Please fill in the fields below!');
+
+        const managerQuestions = () => {
+            inquirer.prompt([{
+                type: 'input',
+                name: 'managerName',
+                message: "Please enter your manager's name",
+                validate: (answer) => {
+                    if (answer) {
+                        return true;   
+                    } else {
+                        console.log( "Enter the manager's name")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the manager's ID number?",
+                validate: (answer) => {
+                    const validAnswer = answer.match(/^[1-9]\d*$/);
+                        if (validAnswer){
+                            return true;
+                        } else {
+                
+                           return 'Enter a number greater than zero.';
+                        }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "Please enter your manager's email",
+                validate: (answer) => {
+                    const validAnswer = answer.match(/\S+@\S+\.\S+/);
+                    if(pass){
+                        return true;
+                    } else {
+                        return 'Please enter a valid email';
+                    };
+                },
+            },
+
+            {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: "Please enter your manager's office number.",
+                validate: (answer) => {
+                    const validAnswer = answer.match(/^[1-9]\d*$/);
+                    if (validAnswer){
+                        return true;
+                    } else {
+                        return 'Please enter a valid office number.';
+                    };
+                },
+            },
+
+            ])
+        }
+
+}
+
+    
